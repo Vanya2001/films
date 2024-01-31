41fc33d89b47ade77214ce1b6e59461a80d0efe5
Volodymyr <ivan@gmail.com>
2024-01-31 14:25:06 +0100

    Закінчення першої частини

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'user strict'
 const numberOfFilms = prompt("Cколько фильмов вы уже посмотрели?","");
-console.log(numberOfFilms);
+let personalMovieDb = {
+  cout : numberOfFilms,
+  movies : {},
+  actors : {},
+  genres : [],
+  private : false
+};
+let films1 = prompt("Один из последних просмотренных фильмов?"),
+    noteFilms1 = prompt("На сколько оцените его?"),
+    films2 = prompt("Один из последних просмотренных фильмов?"),
+    noteFilms2 = prompt("На сколько оцените его?");
+personalMovieDb.movies[films1]=noteFilms1;
+personalMovieDb.movies[films2]=noteFilms2;
+console.log(personalMovieDb);
