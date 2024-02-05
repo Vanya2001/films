3313808065d808d2fdcf9eb8268c6e8f1e0a9065
Volodymyr <ivan@gmail.com>
2024-02-05 10:59:32 +0100

    Замінили два питання одним в фор

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ let personalMovieDb = {
   cout : numberOfFilms,
   movies : {},
   actors : {},
-  genres : [],
+  genres : [],  
   private : false
 };
-let films1 = prompt("Один из последних просмотренных фильмов?"),
+/*let films1 = prompt("Один из последних просмотренных фильмов?"),
     noteFilms1 = prompt("На сколько оцените его?"),
     films2 = prompt("Один из последних просмотренных фильмов?"),
-    noteFilms2 = prompt("На сколько оцените его?");
-personalMovieDb.movies[films1]=noteFilms1;
-personalMovieDb.movies[films2]=noteFilms2;
+    noteFilms2 = prompt("На сколько оцените его?");*/
+
+for(let i = 0;i<2;i++){
+  let films1 = prompt("Один из последних просмотренных фильмов?"),
+    noteFilms1 = prompt("На сколько оцените его?");
+    personalMovieDb.movies[films1]=noteFilms1;
+}
 console.log(personalMovieDb);
