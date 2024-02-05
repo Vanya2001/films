80b030948619ed1539f95c8c35b6a46a451b0e6b
Volodymyr <ivan@gmail.com>
2024-02-05 11:43:09 +0100

    Шкала перегляду фільмів

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let personalMovieDb = {
   private : false
 };
 
-Cycle:for(let i = 0;i<2;i++){
+/*for(let i = 0;i<2;i++){
   let films1 = prompt("Один из последних просмотренных фильмов?"),
     noteFilms1 = prompt("На сколько оцените его?");
     if(films1!=" " && noteFilms1!="" && films1!=null && noteFilms1!=null && films1.length<50){
@@ -17,6 +17,27 @@ Cycle:for(let i = 0;i<2;i++){
       console.log("Erreur");
       i--;
     }
+}*/
+let j = 0;
+do{
+  let films1 = prompt("Один из последних просмотренных фильмов?"),
+    noteFilms1 = prompt("На сколько оцените его?");
+    if(films1!=" " && noteFilms1!="" && films1!=null && noteFilms1!=null && films1.length<50){
+      personalMovieDb.movies[films1]=noteFilms1;
+    }else{
+      console.log("Erreur");
+      j--;
+    }
+    j++;
+}while(j<2);
+if(personalMovieDb.cout<10){
+  alert("Посмотрено довольно мало фильмов");
+}else if(personalMovieDb.cout>=10 && personalMovieDb.cout<=30){
+alert("Вы класный зритель");
+}else if(personalMovieDb.cout>30){
+alert("Вы киноман");
+}else{
+  alert("Произошла ошибка");
 }
 
 console.log(personalMovieDb);
