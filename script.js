44b6519b73320d5b53d87ae7c7fc4ca26222b1a3
Volodymyr <ivan@gmail.com>
2024-02-12 12:30:07 +0100

    Шоб коли ми задавали перше питання воно перевірялося

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 'user strict'
-const numberOfFilms = prompt("Cколько фильмов вы уже посмотрели?","");
+let numberOfFilms;
+function start(){
+  numberOfFilms = prompt("Cколько фильмов вы уже посмотрели?","");
+  while(numberOfFilms==""||numberOfFilms==null||isNaN(numberOfFilms)){
+   numberOfFilms = prompt("Cколько фильмов вы уже посмотрели?","");
+  }
+}
+start();
 let personalMovieDb = {
-  cout : numberOfFilms,
+  count : numberOfFilms,
   movies : {},
   actors : {},
   genres : [],  
   private : false
 };
 
-/*for(let i = 0;i<2;i++){
+for(let i = 0;i<2;i++){
   let films1 = prompt("Один из последних просмотренных фильмов?"),
     noteFilms1 = prompt("На сколько оцените его?");
     if(films1!=" " && noteFilms1!="" && films1!=null && noteFilms1!=null && films1.length<50){
@@ -17,19 +24,7 @@ let personalMovieDb = {
       console.log("Erreur");
       i--;
     }
-}*/
-let j = 0;
-do{
-  let films1 = prompt("Один из последних просмотренных фильмов?"),
-    noteFilms1 = prompt("На сколько оцените его?");
-    if(films1!=" " && noteFilms1!="" && films1!=null && noteFilms1!=null && films1.length<50){
-      personalMovieDb.movies[films1]=noteFilms1;
-    }else{
-      console.log("Erreur");
-      j--;
-    }
-    j++;
-}while(j<2);
+}
 if(personalMovieDb.cout<10){
   alert("Посмотрено довольно мало фильмов");
 }else if(personalMovieDb.cout>=10 && personalMovieDb.cout<=30){
